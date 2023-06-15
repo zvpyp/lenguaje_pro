2e7a182fa21d78ce8496b26c8cd5d4cb2b6d1927
mateo <mateoedgardogomez@gmail.com>
2023-06-14 22:44:28 -0300

    arreglos a plantilla

---
+++ main.py
@@ -0,0 +1,14 @@
+tabla_simbolos = {
+    'var' : 'SECCION_VARIABLES',
+    'body': 'CUERPO', 
+    'for' : 'BUCLE_FOR',
+    'while' : 'BUCLE_WHILE',
+    'if' : 'CONDICIONAL_IF',
+    'else' : 'CONDICIONAL_ELSE',
+    'real' : 'TIPO_REAL',
+    'array' : 'TIPO_ARRAY',
+    'from' : 'FOR_DESDE',
+    'to' : 'FOR_HASTA',
+    'read' : 'LECTURA',
+    'write' : 'ESCRITURA',
+}
