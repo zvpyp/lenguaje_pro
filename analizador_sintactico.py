52a66c7327c8f8b99a050817d8ce60a3a6d34037
Máximo Fernández <maximo03f@gmail.com>
2023-07-10 22:47:00 -0300

    arreglos finales al analizador sintáctico

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -4,14 +4,13 @@ import analizador_lexico as al
 TERMINALES = [
     "var", "{", "}", "id", ":", "," , "real", "array", "[", "]", "body", "=",
     "const_real", ";", "+", "-", "*", "/", "read", "write", "cadena", "(", ")",
-    "if", "operador_relacional", "else", "while", "for", "from", "to", "not", "or", "and", "$"
+    "if", "operador_relacional", "else", "while", "for", "from", "to", "not", "or", "and"
     ]
 
-# NOTA: corregir la TAS y agregar el $
-
 class arbol_9ario:
-    def __init__(self, lexema):
+    def __init__(self, lexema, simbolo):
         self.lexema = lexema
+        self.simbolo = simbolo
         self.hijos = []
     
     def agregar_hijo(self, nodo):
@@ -21,77 +20,96 @@ class arbol_9ario:
         return self.hijos[pos]
     
     def __str__(self):
-        return self.lexema
+        return self.simbolo + f" ({self.lexema})"
     
-    def inorden(self):
+    def preorden(self):
         print(self)
         for hijo in self.hijos:
-            hijo.inorden()
-        input(f'acá terminan los hijos de {self}')
-
-
-dict_tas = {}
-
-pila = [arbol_9ario('$'), arbol_9ario('programa')]
-
-# Crea la TAS en forma de diccionario
-with open('TAS.csv', 'r') as archivo1:
-    csv_tas = csv.reader(archivo1)
-    lista_tas = list(csv_tas)
-
-    for i in range(1, len(lista_tas)):
-        for j in range(1, len(lista_tas[0])):
-            #print(f"i: {i} j:{j}")
-            # Evita los vacios
-            if lista_tas[i][j] == '':
-                continue
-            # diccionario de la forma (variable, terminal) = produccion
-            dict_tas[(lista_tas[i][0],lista_tas[0][j])] = lista_tas[i][j]
-
-
-fuente = open("prueba.txt").read()
-control = 0
-complex = ""
-arbol = pila[1] # la raiz del arbol es siempre "programa"
-
-while complex != '$' and complex != 'ERROR':
-    fuente, control, complex, lexema = al.obtener_siguiente_comp_lex(fuente, control, al.tabla_simbolos)
-    #print(f"{complex}: {complex}")
-
-    # desapilamos el tope de la pila
-
-    tope = pila.pop()
-
-    while tope.lexema not in TERMINALES:
-
-        
-        # obtiene la producción separada de derecha a izquierda según la tupla
-        #print(f"tope: {tope.lexema}")
-        #print(f"complex: {complex}")
-
-        if tope.lexema != "epsilon" and tope.lexema != "":
-            
-
-            elementos_apilados = dict_tas[tope.lexema, complex].split(' ')
-            elementos_apilados.reverse()
-
-            for elemento in elementos_apilados:
-                nodo_aux = arbol_9ario(elemento)
-                pila.append(nodo_aux)
-                tope.agregar_hijo(nodo_aux)
-
-            tope.hijos.reverse()
-
-            #for pos, nodo in enumerate(pila):
-            #    print(f'elemento {pos}: {nodo}')
-
-            tope = pila.pop()
-        else:
-            tope = pila.pop()
-
+            hijo.preorden()
     
-    if tope.lexema != complex:
-        print(f"tope: {tope.lexema}")
-        print(f"complex: {complex}")
-        print("¡¡¡ERROR SINTÁCTICO!!!")
-        break
+    def mostrar_hijos(self):
+        print(f"hijos de {self}:")
+        for hijo in self.hijos:
+            print(hijo)
+
+def analizador_predictivo(ruta_archivo):
+
+    dict_tas = {}
+
+    pila = [arbol_9ario('', '$'), arbol_9ario('', 'programa')]
+
+    # Crea la TAS en forma de diccionario
+    with open('TAS.csv', 'r') as archivo1:
+        csv_tas = csv.reader(archivo1)
+        lista_tas = list(csv_tas)
+
+        for i in range(1, len(lista_tas)):
+            for j in range(1, len(lista_tas[0])):
+                #print(f"i: {i} j:{j}")
+                # Evita los vacios
+                if lista_tas[i][j] != '':
+                    # diccionario de la forma (variable, terminal) = produccion
+                    if lista_tas[i][j] == 'epsilon':
+                        dict_tas[(lista_tas[i][0],lista_tas[0][j])] = ''
+                    else:
+                        dict_tas[(lista_tas[i][0],lista_tas[0][j])] = lista_tas[i][j]
+
+
+    fuente = open(ruta_archivo).read()
+    control = 0
+    complex = ""
+    arbol = pila[1] # la raiz del arbol es siempre "programa"
+
+    estado = 'en proceso'
+
+    fuente, control, complex, lexema = al.obtener_siguiente_comp_lex(fuente, control, al.tabla_simbolos)
+
+    while estado == 'en proceso':
+        #print(f"{complex}: {complex}")
+
+        # desapilamos el tope de la pila
+        tope = pila.pop()
+
+        if tope.simbolo in TERMINALES:
+            if tope.simbolo != complex:
+                estado = f'error: se esperaba {tope.simbolo} y se obtuvo {complex}'
+            else:
+                tope.lexema = lexema
+
+                fuente, control, complex, lexema = al.obtener_siguiente_comp_lex(fuente, control, al.tabla_simbolos)
+        elif tope.simbolo == '$':
+            print('fin')
+            if complex == '$':
+                estado = 'exito'
+            else:
+                estado = f'error: se esperaba fin de archivo y se obtuvo {complex}'
+        else:
+            if (tope.simbolo, complex) in dict_tas:
+                if dict_tas[(tope.simbolo, complex)] != '':
+                    elementos_apilados = dict_tas[(tope.simbolo, complex)].strip().split(' ')
+                    elementos_apilados.reverse()
+
+                    for elemento in elementos_apilados:
+                        if elemento == '':
+                            print(dict_tas[(tope.simbolo, complex)])
+                        nodo_aux = arbol_9ario('', elemento)
+                        pila.append(nodo_aux)
+                        tope.agregar_hijo(nodo_aux)
+
+                    tope.hijos.reverse()
+            else:
+                estado = f'error: desde {tope.simbolo} no se puede derivar una cadena que comience con {complex}'
+
+    return arbol, estado
+
+
+def imprimir_arbol(nodo_raiz, desplazamiento=''):
+    print(desplazamiento, nodo_raiz)
+
+    for hijo in nodo_raiz.hijos:
+        imprimir_arbol(hijo, desplazamiento+'  ')
+
+arbol, estado = analizador_predictivo('prueba.txt')
+
+imprimir_arbol(arbol)
+print(estado)
